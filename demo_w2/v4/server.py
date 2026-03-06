9405fa289ef62f7260c64e9abb86f96ce7f77255
tungluuquang <tungq2005@gmail.com>
2026-03-06 09:08:01 +0700

    v4 Cacheable

---
+++ demo_w2/v4/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request, make_response
+
+app = Flask(__name__)
+
+users = [
+    {"id": 1, "name": "Tung"},
+    {"id": 2, "name": "Nam"}
+]
+
+@app.route("/")
+def home():
+    return "Flask server running"
+
+@app.route("/api/v4/users", methods=["GET"])
+def get_user():
+    user_id = request.headers.get("X-User-ID")
+
+    if not user_id:
+        return jsonify({"error": "User ID header missing"}), 400
+
+    user = next((u for u in users if u["id"] == int(user_id)), None)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    response = make_response(jsonify(user))
+    response.headers["Cache-Control"] = "public, max-age=60"
+
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
