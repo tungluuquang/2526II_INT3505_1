a5cff06f5a7e87def86a7a37b123b1685e3b2cb5
tungluuquang <tungq2005@gmail.com>
2026-04-09 16:46:08 +0700

    showing SQLAlchemy query

--- demo_w6/bvn.py
+++ demo_w6/bvn.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///demo_pagination.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 # MODEL
